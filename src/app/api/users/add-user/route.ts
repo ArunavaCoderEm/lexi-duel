856c634e4bd1d9423aca6bf66e4f73d49c156de8
ArunavaCoderEm <acecankill@gmail.com>
2025-05-21 22:30:32 +0530

    BE init

---
+++ src/app/api/users/add-user/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  console.log(req.body);
+  return NextResponse.json({ message: "POST request received" });
+}
---
+++ src/app/api/users/update-user/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(req: NextRequest) {
+  console.log(req.body);
+  return NextResponse.json({ message: "POST request received" });
+}
