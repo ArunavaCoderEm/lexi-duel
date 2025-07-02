5b0a2e77d2ab73e52d004ff9ae33fbf4664c0974
ArunavaCoderEm <acecankill@gmail.com>
2025-07-03 02:22:25 +0530

    feat: added axios and changing backend to a seperate hono folder

--- client/src/app/api/games/start-game/route.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(req: NextRequest) {
-  console.log(req.body);
-  return NextResponse.json({ message: "PUT request received" });
-}
--- client/src/app/api/games/update-score/route.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-
-import { NextRequest, NextResponse } from "next/server";
-
-export async function PUT(req: NextRequest) {
-  console.log(req.body);
-  return NextResponse.json({ message: "PUT request received" });
-}
